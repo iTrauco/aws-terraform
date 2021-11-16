c64c264c729952b5f2753e933b19a293317a7fb5
Christopher Trauco <chris@trau.co>
2021-11-15 22:40:35 -0500

    refractor db credentials

--- variables.tf
+++ variables.tf
@@ -51,3 +51,15 @@ variable "instance_type" {
   type        = string
   default     = "t2.micro"
 }
+
+variable "db_username" {
+  description = "DB Admin Username"
+  type = 'string'
+  sensitive = true
+}
+
+variable "db_password" {
+  descriptopn = 'DB admin password'
+  type = string
+  sensitive = true
+}
